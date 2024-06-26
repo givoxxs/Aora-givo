3e3f156b7264199f5909a0c8ef5986b05e4973c7
Phan Văn Toàn <toanyogame@gmail.com>
2024-06-26 19:30:29 +0700

    first commit

---
+++ app/(auth)/sign-in.jsx
@@ -0,0 +1,96 @@
+import { Alert, Image, ScrollView, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+import { images } from "../../constants";
+import FormFeild from '../../components/FormFeild';
+import CustomButton from '../../components/CustomButton';
+import { Link, router } from 'expo-router';
+import { getCurrentUser, signIn } from "../../lib/appwrite";
+import { useGlobalContext } from "../../context/GlobalProvider";
+
+const SignIn = () => {
+
+  const { setUser, setIsLoggedIn } = useGlobalContext();
+  
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const [form, setForm] = useState({
+    email: '',
+    password: ''
+  })
+
+
+  const submit = async () => {
+    if (!form.email || !form.password) {
+      Alert.alert("Error", "Please fill in all feilds!");
+    }
+
+    setIsSubmitting(true);
+
+    try {
+      await signIn(form.email, form.password);
+      const result = await getCurrentUser();
+      setUser(result);
+      setIsLoggedIn(true);
+      // set it to global state...
+      Alert.alert("Success", "User signed in succesfully");
+      router.replace('/home');
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="w-full justify-center min-h-[85vh] px-4 my-6">
+          <Image 
+            source={images.logo}
+            resizeMode='contain'
+            className="w-[115px] h-[35px]"
+          />
+
+          <Text className="text-2xl text-white text-semibold mt-10 font-psemibold">Login in to Aora</Text>
+
+          <FormFeild 
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({...form, email: e})}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+          <FormFeild 
+            title="Password"
+            value={form.password}
+            handleChangeText={(e) => setForm({...form, password: e})}
+            otherStyles="mt-7"
+          />
+
+          <CustomButton 
+            title="Sign In"
+            handlePress={submit}
+            containerStyles="mt-7"
+            isLoading={isSubmitting}
+          />
+
+          <View className="justify-center pt-5 flex-row gap-2">
+            <Text className="text-lg text-gray-100 font-pregular">
+              Don't have account?
+            </Text>
+            <Link
+              href="/sign-up"
+              className='text-lg text-secondary font-psemibold'
+            >
+              Sign Up
+            </Link>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  )
+}
+
+export default SignIn
---
+++ app/(tabs)/home.jsx
@@ -0,0 +1,21 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+
+const Home = () => {
+  return (
+    <SafeAreaView>
+      <FlatList 
+        data={[{id: 1}]}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => (
+          <Text>{item.id}</Text>
+        )}
+      />
+    </SafeAreaView>
+  )
+}
+
+export default Home
+
+const styles = StyleSheet.create({})
